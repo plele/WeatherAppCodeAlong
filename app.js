48a4e9d01cb12a19311c26d0dc9e7188cb006f9d
Prerna Lele <plele@mediamath.com>
2020-02-25 23:34:41 -0500

    Adding logic to display temp and icons

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span')
+
+
     if( navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const api = `
-            https://api.darksky.net/forecast/06d147634a2bb275f6ef9f0ec5769b50/${lat},${long}`
-        })
-    fetch(api)
 
-    }else {
-        h1.textContent = "Hey This is not working"
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/06d147634a2bb275f6ef9f0ec5769b50/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                }  )
+                .then(data => {
+                    console.log(data);
+                    const {temperature, summary, icon} = data.currently;
+                    //set dom elements with the data we got back from API 
+                    temperatureDegree.textContent = temperature ;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone ;
+                    //Formula for celcius
+                    let celcius = (temperature - 32)* 5/9 ;
+                    //set Icon
+                    setIcons(icon, document.querySelector('.icon') )
+                    
+                    //change temp from F to C
+                    temperatureSection.addEventListener('click', () =>{
+                        if(temperatureSpan.textContent === 'F') {
+                            temperatureSpan.textContent = 'C';
+                            temperatureDegree.textContent = Math.floor(celcius) ;
+                        } else {
+                            temperatureSpan.textContent = 'F';
+                            temperatureDegree.textContent = temperature; 
+                        }
+                    })
+
+
+            });
+        });
+
+    }
+    
+    function setIcons(icon , iconID) {
+        const skycons  = new Skycons({color : "white"});
+        const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+        skycons.play();
+        return skycons.set (iconID, Skycons[currentIcon]);
+
     }
 
 });
